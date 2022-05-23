de2f08e4a530c4c1e503fea3fc2811f196b8d9e3
Neto <Luiz_Tomasi@edu.univali.br>
2022-05-23 04:31:59 -0300

    CM- Arrumado o nome do packet repository

--- cliente-service/src/main/java/br/com/saks/clienteservice/repository/ClienteRepository.java
+++ cliente-service/src/main/java/br/com/saks/clienteservice/repository/ClienteRepository.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package br.com.saks.usuarioservice.repository;
+package br.com.saks.clienteservice.repository;
 
 import br.com.saks.clienteservice.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
