bbce04cf04d9868f2c1df6ae122e413a974eb6ef
7915772 <7915772@U702610.univali.br>
2022-05-23 15:53:54 -0300

    CM - Criado o repository

---
+++ administrador-service/src/main/java/br/com/saks/administradorservice/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.saks.administradorservice.repository;
+
+import br.com.saks.administradorservice.model.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author 7915772
+ */
+public interface ClienteRepository extends JpaRepository<Administrador, Long> {
+    
+}
