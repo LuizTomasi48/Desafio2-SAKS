d94bc442dd3e5bbc18fb58456208aeb940c391b6
7915772 <7915772@U702610.univali.br>
2022-05-23 17:41:36 -0300

    CM - Criado o service tipoImovel-service

---
+++ tipoImovel-service/tipoImovel-service/src/main/java/br/com/saks/tipoImovelservice/TipoImovelServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.saks.tipoImovelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipoImovelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipoImovelServiceApplication.class, args);
+	}
+
+}
---
+++ tipoImovel-service/tipoImovel-service/src/test/java/br/com/saks/tipoImovelservice/TipoImovelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saks.tipoImovelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipoImovelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
