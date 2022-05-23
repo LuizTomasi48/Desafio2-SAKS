de8a58413acbca62e76569f3a4064938b0fdff02
Neto <Luiz_Tomasi@edu.univali.br>
2022-05-23 12:51:12 -0300

    CM- Criado o administrador-service

---
+++ administrador-service/src/main/java/br/com/saks/administradorservice/AdministradorServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.saks.administradorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorServiceApplication.class, args);
+	}
+
+}
