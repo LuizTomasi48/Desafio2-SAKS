f0e0e35b3405dbb3913efc386b751a0d743fa42d
Neto <Luiz_Tomasi@edu.univali.br>
2022-05-24 20:24:08 -0300

    CM- Criado o controller

---
+++ tipoImovel-service/tipoImovel-service/src/main/java/br/com/saks/tipoImovelservice/controller/TipoImovelController.java
@@ -0,0 +1,14 @@
+package br.com.saks.tipoImovelservice.controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Bolofofo
+ */
+public class TipoImovelController {
+    
+}
