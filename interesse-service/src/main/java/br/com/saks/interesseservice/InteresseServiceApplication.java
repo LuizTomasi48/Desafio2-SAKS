71ff3c41fb73a509217daee4f714a6e981e6952d
7915772 <7915772@U702619.univali.br>
2022-05-25 16:22:19 -0300

    CM- Criado o interesse-service

---
+++ interesse-service/src/main/java/br/com/saks/interesseservice/InteresseServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.saks.interesseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteresseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteresseServiceApplication.class, args);
+	}
+
+}
