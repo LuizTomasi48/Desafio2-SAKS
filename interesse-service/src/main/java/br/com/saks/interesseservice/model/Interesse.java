dd56450da6a742e7c64da1f9eb113c77c8616250
7915772 <7915772@U702619.univali.br>
2022-05-25 16:30:23 -0300

    CM- Criado o model

---
+++ interesse-service/src/main/java/br/com/saks/interesseservice/model/Interesse.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.saks.interesseservice.model;
+
+/**
+ *
+ * @author 7915772
+ */
+public class Interesse {
+    
+}
